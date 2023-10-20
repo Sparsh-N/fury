0992a8947360994e310b1827eb63415c94465004
Luke Nargang <luciano.nargang@gmail.com>
2023-10-20 15:20:50 -0400

    Revert "Update antialiasing.py"
    This reverts commit 8a51d87215a568fad5a5eeb81d3ad9f9a5ae371e.

--- docs/experimental/viz_anti_aliasing.py
+++ docs/experimental/viz_anti_aliasing.py
@@ -3,7 +3,6 @@ import numpy.testing as npt
 from fury import actor
 from fury import primitive as fp
 from fury import shaders, window
-from fury.lib import (RenderWindow)
 
 
 def simulated_bundle(no_streamlines=10, waves=False):
@@ -18,6 +17,8 @@ def simulated_bundle(no_streamlines=10, waves=False):
         bundle.append(pts)
 
     return bundle
+
+
 
 def test_streamtube_and_line_actors():
     scene = window.Scene()
@@ -43,9 +44,7 @@ def test_streamtube_and_line_actors():
     scene.add(c2)
     scene.add(bundle_actor)
 
-    # window.antialiasing(scene, window.ShowManager.window, multi_samples=0)
-    
-    window.show(scene, multi_samples=10000, order_transparent=True,occlusion_ratio=1.0)
+    window.show(scene)
 
     db = 1
 
